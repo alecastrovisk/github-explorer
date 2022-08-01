2bac8ef28f487f44e0b69b76aaafcdf011b9d297
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-08-01 10:09:32 -0300

    feat: Dashboard complete

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -42,6 +42,8 @@ export function Dashboard() {
      * - call addRepository function sending inputText value;
      * - clean inputText value.
      */
+    addRepository(inputText);
+    setInputText('');
   }
 
   function handleRepositoryPageNavigation(id: number) {
@@ -53,6 +55,7 @@ export function Dashboard() {
      *  repositoryId: id of the repository
      * })
      */
+    navigate('Repository', { repositoryId: id });
   }
 
   return (
@@ -66,6 +69,7 @@ export function Dashboard() {
               ref={inputRef}
               placeholder="Digite aqui 'usuário/repositório'"
               value={inputText}
+              onChangeText={setInputText}
               /**
                * TODO - update inputText value when input text value 
                * changes:
@@ -80,6 +84,7 @@ export function Dashboard() {
             <InputButton
               testID="input-button"
               onPress={handleAddRepository}
+              disabled={inputText === ''}
               /**
                * TODO - ensure to disable button when inputText is 
                * empty (use disabled prop to this):
